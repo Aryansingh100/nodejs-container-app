df0bf27c538840e49481c03be77c657bbdf1b0e4
Aryan <sin14784@sheridancollege.ca>
2025-11-14 11:04:26 -0500

    test 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => 
     {
-    res.send('Hello, Dockerized Node.js App!');
+    res.send('Hello, Dockerized Node.js App from Aryan!');
     }
 );
 app.listen(PORT, () => {
